c2eb6d5b89e002b1b75ad2227499e607c3cfe93c
rileythejones <rileymjones@gmail.com>
2020-05-15 05:05:14 -0600

    mvp

--- adv.py
+++ adv.py
@@ -1,13 +1,11 @@
 from room import Room
 from player import Player
 from world import World
-
 import random
 from ast import literal_eval
 
 # Load world
 world = World()
-
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -26,10 +24,14 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
 
+""" solution """
 
+player = Player(world.starting_room)
+player.traverse(len(world.rooms))
+traversal_path = list(player.path)
+
+""" -------- """
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
@@ -46,17 +48,15 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- player.py
+++ player.py
@@ -1,11 +1,58 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class Player:
-    def __init__(self, starting_room):
-        self.current_room = starting_room
-    def travel(self, direction, show_rooms = False):
-        next_room = self.current_room.get_room_in_direction(direction)
-        if next_room is not None:
-            self.current_room = next_room
-            if (show_rooms):
-                next_room.print_room_description(self)
+    def __init__(self, start):
+        self.current_room = start
+        self.visited = set()
+        self.path = list()
+        
+    def travel(self, d):
+        """print("TRAVERSE")"""
+        n = self.current_room = self.current_room.get_room_in_direction(d)
+        if n:
+            self.visited.add(self.current_room.id)
+            self.path.append(d)
+            
         else:
             print("You cannot move in that direction.")
+            
+            
+    def bfs(self):
+        """print("BFS")"""
+        q = Queue()
+        q.enqueue([self.current_room, list()])
+        
+        searched = set()
+
+        while q.size() > 0:
+            room, path = q.dequeue()
+  
+            if room.id not in searched:
+                searched.add(room.id)      
+                for d in room.get_exits():
+                    copy = list(path)
+                    copy.append(d)
+                    n = room.get_room_in_direction(d)
+                    q.enqueue([n, copy])
+                        
+            if room.id not in self.visited:
+                return path
+        
+        
+    def traverse(self, n):
+        self.visited.add(self.current_room.id)
+        while len(self.visited) < n :
+            path = self.bfs()
+            for d in path:
+                self.travel(d)
